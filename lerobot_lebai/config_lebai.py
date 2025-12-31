b3402ad0318bd48c79fbb984d04ba2f05d242e27
rise0chen <rise0chen@163.com>
2025-12-31 11:26:15 +0800

    lebai

--- lerobot_lebai/config_lebai.py
+++ lerobot_lebai/config_lebai.py
@@ -6,11 +6,14 @@ from lerobot.robots.config import RobotConfig
 
 @RobotConfig.register_subclass("lerobot_lebai")
 @dataclass
-class XarmConfig(RobotConfig):
+class LebaiConfig(RobotConfig):
     ip: str = "192.168.1.184"
+    port: int = 3031
+    gripper_force: int = 100
+    a: int = 1
+    v: int = 1
+    r: int = 0
     use_effort: bool = False
     use_velocity: bool = True
     use_acceleration: bool = True
-    home_translation: list[float] = field(default_factory=lambda: [0.2, 0.0, 0.05])
-    home_orientation_euler: list[float] = field(default_factory=lambda: [3.14, 0.0, 0.0])
     cameras: dict[str, CameraConfig] = field(default_factory=dict)
--- lerobot_lebai/lebai.py
+++ lerobot_lebai/lebai.py
@@ -7,14 +7,13 @@ from typing import Any
 import numpy as np
 import transforms3d as t3d
 import lebai_sdk
-from teleop.utils.jacobi_robot import JacobiRobot
 
 from lerobot.cameras import make_cameras_from_configs
 from lerobot.utils.errors import DeviceNotConnectedError, DeviceAlreadyConnectedError
 from lerobot.robots.robot import Robot
 
 from .config_lebai import LebaiConfig
-
+JOINT_NUM = 6
 
 logger = logging.getLogger(__name__)
 
@@ -30,47 +29,26 @@ class Lebai(Robot):
         this_dir = os.path.dirname(os.path.abspath(__file__))
 
         self.config = config
-        self._is_connected = False
         self._arm = None
-        self._gripper = None
-        self._jacobi = JacobiRobot(
-            os.path.join(this_dir, "lite6.urdf"), ee_link="link_tool"
-        )
         self._initial_pose = None
-        self._prev_observation = None
 
     def connect(self, calibrate: bool = True) -> None:
         if self.is_connected:
             raise DeviceAlreadyConnectedError(f"{self} already connected")
 
-        # Connect to the robot and set it to a servo mode
-        self._arm = XArmAPI("192.168.1.184", is_radian=True)
-        self._arm.connect()
-        self._arm.motion_enable(enable=True)
-        self._arm.set_mode(1)  # Position mode
-        self._arm.set_state(state=0)  # Sport state
-
-        # Initialize gripper
-        self._gripper = Lite6Gripper(self._arm)
-
-        # set joint positions to jacobi, read from arm
-        code, joint_positions = self._arm.get_servo_angle()
-        if code != 0:
-            raise DeviceNotConnectedError(f"Failed to get joint angles from {self}")
-        for i in range(1, 7):
-            joint_name = f"joint{i}"
-            self._jacobi.set_joint_position(joint_name, joint_positions[i - 1])
+        # Connect to the robot
+        self._arm = lebai_sdk.connect(self.config.ip, self.config.port)
+        self._arm.start_sys()
 
         for cam in self.cameras.values():
             cam.connect()
 
-        self.is_connected = True
         self.configure()
         logger.info(f"{self} connected.")
 
     @property
     def _motors_ft(self) -> dict[str, type]:
-        motors = {f"joint{i}.pos": float for i in range(1, 7)}
+        motors = {f"joint{i}.pos": float for i in range(1, JOINT_NUM + 1)}
         motors["gripper.pos"] = float
         return motors
 
@@ -85,15 +63,8 @@ class Lebai(Robot):
         action = copy.deepcopy(action)
 
         if "delta_x" in action and "delta_y" in action and "delta_z" in action:
-            pose = self._jacobi.get_ee_pose()
-            delta_pose = np.eye(4)
-            delta_pose[:3, 3] = [
-                action["delta_x"],
-                action["delta_y"],
-                action["delta_z"],
-            ]
-
-            delta_rotation = None
+            pose = self._arm.get_kin_data().actual_tcp_pose
+            target = copy.deepcopy(pose)
             if (
                 "delta_roll" in action
                 and "delta_pitch" in action
@@ -102,57 +73,37 @@ class Lebai(Robot):
                 roll = action["delta_roll"]
                 pitch = action["delta_pitch"]
                 yaw = action["delta_yaw"]
-                delta_rotation = t3d.euler.euler2mat(roll, pitch, yaw)
-                delta_pose[:3, :3] = delta_rotation
+                target = lebai:pose_trans(pose, {'x' : 0, 'y' : 0, 'z' : 0, 'rz' : roll, 'ry' : pitch, 'rx' : yaw} )
 
-            action["pose"] = np.eye(4)
-            if delta_rotation is not None:
-                action["pose"][:3, :3] = delta_rotation @ pose[:3, :3]
-            else:
-                action["pose"][:3, :3] = pose[:3, :3]
-            action["pose"][:3, 3] = pose[:3, 3] + delta_pose[:3, 3]
-
-        if "pose_from_initial" in action:
-            delta_pose = action["pose_from_initial"]
-            action["pose"] = np.eye(4)
-            action["pose"][:3, :3] = delta_pose[:3, :3] @ self._initial_pose[:3, :3]
-            action["pose"][:3, 3] = self._initial_pose[:3, 3] + delta_pose[:3, 3]
-
-        if "home" in action:
-            home_pose = t3d.affines.compose(
-                self.config.home_translation,
-                t3d.euler.euler2mat(*self.config.home_orientation_euler),
-                [1.0, 1.0, 1.0],
-            )
-            action["pose"] = home_pose
-            self._initial_pose = self._jacobi.get_ee_pose()
+            target['x']=target['x']+action["delta_x"]
+            target['y']=target['y']+action["delta_y"]
+            target['z']=target['z']+action["delta_z"]
+            self._arm.movel(target, self.config.a, self.config.v, 0, self.config.r)
+            return action
 
         if "pose" in action:
             pose = action["pose"]
-            self._jacobi.servo_to_pose(pose)
-
-            # Get joint positions from Jacobi
-            for i in range(1, 7):
-                joint_pos = self._jacobi.get_joint_position(f"joint{i}")
-                action[f"joint{i}.pos"] = joint_pos
+            xyz = pose[:3, 3]
+            rpy = t3d.euler.mat2euler(pose[:3, :3])
+            target = {'x' : xyz[0], 'y' : xyz[1], 'z' : xyz[2], 'rz' : rpy[0], 'ry' : rpy[1], 'rx' : rpy[2]}
+            self._arm.movel(pose, self.config.a, self.config.v, 0, self.config.r)
+            return action
 
         # Execute joint positions
         if "joint1.pos" in action:
             joint_positions = []
-            for i in range(1, 7):
+            for i in range(1, JOINT_NUM + 1):
                 joint_pos = action[f"joint{i}.pos"]
                 joint_positions.append(joint_pos)
 
-            self._arm.set_servo_angle_j(joint_positions)
-        else:
-            for i in range(1, 7):
-                action[f"joint{i}.pos"] = self._jacobi.get_joint_position(f"joint{i}")
+            self._arm.movej(joint_positions, self.config.a, self.config.v, 0, self.config.r)
+            return action
 
         # Send gripper command
         if "gripper.pos" in action or "gripper" in action:
             gripper_pos = action.get("gripper.pos", action.get("gripper", 0.0))
-            self._gripper.set_gripper_state(gripper_pos)
-            action["gripper.pos"] = self._gripper.get_gripper_state()
+            self._arm.set_claw(self.config.gripper_force, gripper_pos)
+            return action
 
         return action
 
@@ -164,34 +115,15 @@ class Lebai(Robot):
         start = time.perf_counter()
 
         # Read joint positions
-        _, (joint_angles, _, joint_efforts) = self._arm.get_joint_states()
+        kin_data = self._arm.get_kin_data()
 
         obs_dict = {}
-        for i, angle in enumerate(joint_angles[:6]):  # First 6 angles are joints
+        for i, angle in enumerate(kin_data.actual_joint_pose):
             obs_dict[f"joint{i+1}.pos"] = angle
-            obs_dict[f"joint{i+1}.effort"] = joint_efforts[i]
-        obs_dict["gripper.pos"] = self._gripper.get_gripper_state()
-
-        # calculate joint velocities
-        for i in range(1, 7):
-            joint_name = f"joint{i}"
-            if self._prev_observation is not None:
-                prev_angle = self._prev_observation[f"{joint_name}.pos"]
-                curr_angle = obs_dict[f"{joint_name}.pos"]
-                obs_dict[f"{joint_name}.vel"] = (
-                    curr_angle - prev_angle
-                ) * 60.0  # Assuming 60Hz control frequency
-            else:
-                obs_dict[f"{joint_name}.vel"] = 0.0
-
-        # calculate joint accelerations
-        for i in range(1, 7):
-            joint_name = f"joint{i}"
-            if self._prev_observation is not None:
-                prev_vel = self._prev_observation[f"{joint_name}.vel"]
-                obs_dict[f"{joint_name}.acc"] = obs_dict[f"{joint_name}.vel"] - prev_vel
-            else:
-                obs_dict[f"{joint_name}.acc"] = 0.0
+            obs_dict[f"joint{i+1}.effort"] = kin_data.actual_joint_torque[i]
+            obs_dict[f"joint{i+1}.vel"] = kin_data.actual_joint_speed[i]
+            obs_dict[f"joint{i+1}.acc"] = kin_data.actual_joint_acc[i]
+        obs_dict["gripper.pos"] = self._arm.get_claw().amplitude
 
         # Capture images from cameras
         for cam_key, cam in self.cameras.items():
@@ -200,29 +132,22 @@ class Lebai(Robot):
             dt_ms = (time.perf_counter() - start) * 1e3
             logger.debug(f"{self} read {cam_key}: {dt_ms:.1f}ms")
 
-        self._prev_observation = obs_dict
         return obs_dict
 
     def reset(self):
-        if self._gripper:
-            self._gripper.reset_gripper()
+        self._arm.init_claw(True)
 
     def disconnect(self) -> None:
         if not self.is_connected:
             return
 
-        if self._gripper is not None:
-            self._gripper.stop()
-            self._gripper = None
-
         if self._arm is not None:
-            self._arm.disconnect()
+            self._arm.stop_sys()
             self._arm = None
 
         for cam in self.cameras.values():
             cam.disconnect()
 
-        self.is_connected = False
         logger.info(f"{self} disconnected.")
 
     def calibrate(self) -> None:
@@ -232,21 +157,15 @@ class Lebai(Robot):
         if not self.is_connected:
             raise DeviceNotConnectedError(f"{self} is not connected.")
 
-        self._initial_pose = self._jacobi.get_ee_pose()
-        xyz = self._initial_pose[:3, 3]
-        rpy = t3d.euler.mat2euler(self._initial_pose[:3, :3])
-        print(f"Initial pose: xyz={xyz}, rpy={rpy}")
+        self._initial_pose = self._arm.get_kin_data().actual_tcp_pose
+        print(f"Initial pose: {self._initial_pose}")
 
     def is_calibrated(self) -> bool:
         return self.is_connected
 
     @property
     def is_connected(self) -> bool:
-        return self._is_connected
-
-    @is_connected.setter
-    def is_connected(self, value: bool) -> None:
-        self._is_connected = value
+        return self._arm is not None and self._arm.is_connected()
 
     @property
     def _cameras_ft(self) -> dict[str, tuple]:
@@ -259,13 +178,13 @@ class Lebai(Robot):
     def observation_features(self) -> dict[str, Any]:
         features = {**self._motors_ft, **self._cameras_ft}
         if self.config.use_effort:
-            for i in range(1, 7):
+            for i in range(1, JOINT_NUM + 1):
                 features[f"joint{i}.effort"] = float
         if self.config.use_velocity:
-            for i in range(1, 7):
+            for i in range(1, JOINT_NUM + 1):
                 features[f"joint{i}.vel"] = float
         if self.config.use_acceleration:
-            for i in range(1, 7):
+            for i in range(1, JOINT_NUM + 1):
                 features[f"joint{i}.acc"] = float
         return features
 
